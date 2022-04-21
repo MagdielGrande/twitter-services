3f3bb10d11ab9ae5803539be15cf19fbf8b1173f
Magdiel Hermosillo Grande <magdielhgra@gmail.com>
2022-04-20 19:32:32 -0500

    quitando comentarios

--- app/models/user.js
+++ app/models/user.js
@@ -1,33 +1,3 @@
-/*
-Requerimiento 1
- class User{
-    constructor(id, username, name, bio, dateCreated, lastUpdated){
-        this.id = id;
-        this.username = username;
-        this.name = name;
-        this.bio = bio;
-        this.dateCreated = dateCreated;
-        this.lastUpdated = lastUpdated;
-    }
-}
-
-module.exports = User */
-
-//Requerimiento 2
-/* class User{
-    constructor(id, username, name, bio){
-        this.id = id;
-        this.username = username;
-        this.name = name;
-        this.bio = bio;
-        this.dateCreated = new Date();
-        this.lastUpdated = new Date();
-    }
-}
-
-module.exports = User */
-
-//Requerimiento 3
 class User{
     constructor(id, username, name, bio){
         this.id = id;
@@ -58,7 +28,3 @@ class User{
 }
 
 module.exports = User
-
-
-//Requerimiento 4: Cada objeto necesita los siguientes setters: `setUsername` y `setBio`, para actualizar dichas propiedades.
-//Solución: Agrega los setters usando la palabra `set` y modificando el atributo indicado.
